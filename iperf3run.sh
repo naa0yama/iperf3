34e5a3a7191d7789d3cb5f5ed344b40e028ea0bc
Naoki Aoyama <9667078+naa0yama@users.noreply.github.com>
2024-04-10 21:46:25 +0900

    Update iperf3run.sh

--- iperf3run.sh
+++ iperf3run.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ -z "$1" ]; then
     echo "Comment is required."
-    echo "Usage: $(basename "${0}") comment [ all | tcp | udp | th ] (192.0.2.1)"
+    echo "Usage: $(basename "${0}") comment [ all | tcp | udp | th | th_byte ] (192.0.2.1)"
     exit 1
 fi
 
@@ -22,7 +22,7 @@ INTERVAL_SECOND=60
 PARALLEL_INT=5
 WAIT_SECOND=5
 TEST_TIME=$(date '+%Y-%m-%dT%H%M')
-mapfile -t THROUGHPUT_BANDWIDTH < <(seq 5 5 70)
+mapfile -t THROUGHPUT_BANDWIDTH < <(seq 100 10 240)
 MTUS=(128 146 256 354 512 1024 1280 1360 1400 1440 1460 1500)
 CMD_OPTIONS=(
   "--json"
@@ -65,7 +65,8 @@ function time_calc() {
       local eta_time_int_tcp=$(( ${#MTUS[@]} * $(( INTERVAL_SECOND + WAIT_SECOND )) * 2 ))
       local eta_time_int_udp=$(( ${#MTUS[@]} * $(( INTERVAL_SECOND + WAIT_SECOND )) * 2 ))
       local eta_time_int_th=$(( ${#THROUGHPUT_BANDWIDTH[@]} * $(( INTERVAL_SECOND + WAIT_SECOND )) * 2 ))
-      eta_time_int=$(( eta_time_int_tcp + eta_time_int_udp + eta_time_int_th ))
+      local eta_time_int_th_byte=$(( ${#MTUS[@]} * $(( INTERVAL_SECOND + WAIT_SECOND )) * 2 ))
+      eta_time_int=$(( eta_time_int_tcp + eta_time_int_udp + eta_time_int_th + eta_time_int_th_byte ))
 
       ;;
     ("tcp")
@@ -81,6 +82,11 @@ function time_calc() {
     ("th")
       local eta_time_int_th=$(( ${#THROUGHPUT_BANDWIDTH[@]} * $(( INTERVAL_SECOND + WAIT_SECOND )) * 2 ))
       eta_time_int=$(( eta_time_int_th ))
+
+      ;;
+    ("th_byte")
+      local eta_time_int_th_byte=$(( ${#MTUS[@]} * $(( INTERVAL_SECOND + WAIT_SECOND )) * 2 ))
+      eta_time_int=$(( eta_time_int_th_byte ))
 
       ;;
     (*)
@@ -202,7 +208,7 @@ function gen_csv_th() {
     ([.end.streams[]?.sender?.min_rtt] | add),
     ([.end.streams[]?.sender?.mean_rtt] | add),
     ([.end.streams[]?.sender?.max_rtt] | add),
-    .start.version] | @csv' TH*_TCP.json | sort -n >> results.csv
+    .start.version] | @csv' TH*M_TCP.json | sort -n >> results.csv
   sleep 2
 
 
@@ -218,7 +224,42 @@ function gen_csv_th() {
     ([.end.streams[] | select(.udp.sender == true) | .udp?.bits_per_second*0.000001] | add),
     ([.end.streams[] | .udp?.jitter_ms] | add),
     "", "", "", "",
-    .start.version] | @csv' TH*_UDP.json | sort -n >> results.csv
+    .start.version] | @csv' TH*M_UDP.json | sort -n >> results.csv
+  sleep 2
+}
+
+function gen_csv_th_byte() {
+  # TCP TH byte
+  jq --arg COMMENT "${COMMENT}" -r '[
+    $COMMENT,
+    .title,
+    .start.test_start.protocol,
+    1460,
+    .start.test_start.num_streams,
+    .start.test_start.duration,
+    ([.end.streams[] | select(.sender.sender == true) | .sender?.bits_per_second*0.000001] | add),
+    ([.end.streams[] | select(.receiver.sender == false) | .receiver?.bits_per_second*0.000001] | add),
+    "","",
+    ([.end.streams[]?.sender?.min_rtt] | add),
+    ([.end.streams[]?.sender?.mean_rtt] | add),
+    ([.end.streams[]?.sender?.max_rtt] | add),
+    .start.version] | @csv' TH_TCP_MTU*.json | sort -n >> results.csv
+  sleep 2
+
+
+  # UDP TH byte
+  jq --arg COMMENT "${COMMENT}" -r '[
+    $COMMENT,
+    .title,
+    .start.test_start.protocol,
+    .start.test_start.blksize,
+    .start.test_start.num_streams,
+    .start.test_start.duration,
+    ([.end.streams[] | select(.udp.sender == false) | .udp?.bits_per_second*0.000001] | add),
+    ([.end.streams[] | select(.udp.sender == true) | .udp?.bits_per_second*0.000001] | add),
+    ([.end.streams[] | .udp?.jitter_ms] | add),
+    "", "", "", "",
+    .start.version] | @csv' TH_UDP_MTU*.json | sort -n >> results.csv
   sleep 2
 }
 
@@ -244,6 +285,10 @@ function gen_csv() {
       gen_csv_th
 
       ;;
+    ("th_byte")
+      gen_csv_th_byte
+
+      ;;
     (*)
       exit 1
       ;;
@@ -254,14 +299,14 @@ function iperf3_tcp() {
   for i in "${MTUS[@]}"
   do
     local __mtu="0000${i}"
-    echo -e "TCP MTU: ${__mtu: -4}\n\n"
+    echo -e "================================================================================\n\n"
+    echo -e "= TCP MTU: ${__mtu: -4}\n\n"
     set -x
     # Client -> Server
     iperf3 "${CMD_OPTIONS[@]}" -c "${IP}" -t "${INTERVAL_SECOND}" -M $(( i - 40 )) \
       -T "$(date '+%Y-%m-%d %H:%M') TCP UP MTU${__mtu: -4}" --logfile "TCP_UP_MTU${__mtu: -4}.json"
     set +x
     wait_interval
-    echo -e "==============\n\n"
 
     set -x
     # Server -> Client
@@ -269,7 +314,6 @@ function iperf3_tcp() {
       -T "$(date '+%Y-%m-%d %H:%M') TCP DL MTU${__mtu: -4}" --logfile "TCP_DL_MTU${__mtu: -4}.json" -R
     set +x
     wait_interval
-    echo -e "==============\n\n"
   done
 }
 
@@ -277,14 +321,14 @@ function iperf3_udp() {
   for i in "${MTUS[@]}"
   do
     local __mtu="0000${i}"
-    echo -e "UDP MTU: ${__mtu: -4}\n\n"
+    echo -e "================================================================================\n\n"
+    echo -e "= UDP MTU: ${__mtu: -4}\n\n"
     set -x
     # Client -> Server
     iperf3 "${CMD_OPTIONS[@]}" -c "${IP}" -t "${INTERVAL_SECOND}" -l $(( i - 28 )) \
       -T "$(date '+%Y-%m-%d %H:%M') UDP UP MTU${__mtu: -4}" -u -b 200M --logfile "UDP_UP_MTU${__mtu: -4}.json"
     set +x
     wait_interval
-    echo -e "==============\n\n"
 
     set -x
     # Server -> Client
@@ -292,7 +336,6 @@ function iperf3_udp() {
       -T "$(date '+%Y-%m-%d %H:%M') UDP DL MTU${__mtu: -4}" -u -b 200M --logfile "UDP_DL_MTU${__mtu: -4}.json" -R
     set +x
     wait_interval
-    echo -e "==============\n\n"
   done
 }
 
@@ -300,14 +343,14 @@ function iperf3_th() {
   for th_bandwidth in "${THROUGHPUT_BANDWIDTH[@]}"
   do
     local __th_bandwidth="0000$(( th_bandwidth * PARALLEL_INT ))"
-    echo -e "TCP throughput: ${__th_bandwidth: -4}M\n\n"
+    echo -e "================================================================================\n\n"
+    echo -e "= TCP throughput: ${__th_bandwidth: -4}M\n\n"
     set -x
     iperf3 "${CMD_OPTIONS[@]}" --bidir -c "${IP}" -t "${INTERVAL_SECOND}" -M $(( 1500 - 40 )) \
       -T "$(date '+%Y-%m-%d %H:%M') TCP TH${__th_bandwidth: -4}M" \
       -b "${th_bandwidth}M" --logfile "TH${__th_bandwidth: -4}M_TCP.json"
     set +x
     wait_interval
-    echo -e "==============\n\n"
 
     echo -e "UDP throughput: ${__th_bandwidth: -4}M\n\n"
     set -x
@@ -316,7 +359,26 @@ function iperf3_th() {
       -b "${th_bandwidth}M" --logfile "TH${__th_bandwidth: -4}M_UDP.json"
     set +x
     wait_interval
-    echo -e "==============\n\n"
+  done
+}
+
+function iperf3_th_byte() {
+  for i in "${MTUS[@]}"
+  do
+    local __mtu="0000${i}"
+    echo -e "================================================================================\n\n"
+    echo -e "= TH Byte MTU: ${__mtu: -4}\n\n"
+    set -x
+    iperf3 "${CMD_OPTIONS[@]}" --bidir -c "${IP}" -t "${INTERVAL_SECOND}" -M $(( i - 40 )) \
+      -T "$(date '+%Y-%m-%d %H:%M') TCP TH UP MTU${__mtu: -4}" --logfile "TH_TCP_MTU${__mtu: -4}.json"
+    set +x
+    wait_interval
+
+    set -x
+    iperf3 "${CMD_OPTIONS[@]}" --bidir -c "${IP}" -t "${INTERVAL_SECOND}" -l $(( i - 28 )) \
+      -T "$(date '+%Y-%m-%d %H:%M') UDP TH DL MTU${__mtu: -4}" -u --logfile "TH_UDP_MTU${__mtu: -4}.json"
+    set +x
+    wait_interval
   done
 }
 
@@ -329,6 +391,7 @@ function main() {
       iperf3_tcp
       iperf3_udp
       iperf3_th
+      iperf3_th_byte
       gen_csv "${ARGS}"
       ;;
 
@@ -347,8 +410,13 @@ function main() {
       gen_csv "${ARGS}"
       ;;
 
+    ("th_byte")
+      iperf3_th_byte
+      gen_csv "${ARGS}"
+      ;;
+
     (*)
-      echo "Error: type emum [ all | tcp | udp | th ]"
+      echo "Error: type emum [ all | tcp | udp | th | th_byte ]"
       exit 1
       ;;
   esac
